ba83179edbf199953d5ba582e7fddeae82c48e78
Victor Zhao <819413991zhao@gmail.com>
2024-07-01 15:04:47 +0800

    Retrieve folders function.

--- Code.js
+++ Code.js
@@ -1,57 +1,8 @@
-function doGet() {
-  return HtmlService.createHtmlOutputFromFile('Index')
-      .setTitle('Folder Management App')
-      .setWidth(600)
-      .setHeight(400);
-}
-
-function getFolders() {
-  var folders = DriveApp.getFolders();
-  var folderList = [];
-  
+function retrieveFolders(){
+  let folders = DriveApp.getFolders();
   while (folders.hasNext()) {
-    var folder = folders.next();
-    folderList.push({
-      id: folder.getId(),
-      name: folder.getName()
-    });
-  }
-  
-  return folderList;
-}
-
-function duplicateFolder(folderId, newFolderName, keepFilesFolders, maintainAccessFolders) {
-  var originalFolder = DriveApp.getFolderById(folderId);
-  var newFolder = originalFolder.copy(newFolderName);
-  clearFolder(newFolder, keepFilesFolders, maintainAccessFolders);
-  
-  return newFolder.getId();
-}
-
-function clearFolder(folder, keepFilesFolders, maintainAccessFolders) {
-  var folders = folder.getFolders();
-  
-  while (folders.hasNext()) {
-    var subFolder = folders.next();
-    if (!keepFilesFolders.includes(subFolder.getId())) {
-      var files = subFolder.getFiles();
-      while (files.hasNext()) {
-        var file = files.next();
-        file.setTrashed(true);
-      }
-    }
-    
-    if (!maintainAccessFolders.includes(subFolder.getId())) {
-      var editors = subFolder.getEditors();
-      var viewers = subFolder.getViewers();
-      
-      for (var i = 0; i < editors.length; i++) {
-        subFolder.removeEditor(editors[i].getEmail());
-      }
-      
-      for (var j = 0; j < viewers.length; j++) {
-        subFolder.removeViewer(viewers[j].getEmail());
-      }
-    }
+    let folder = folders.next();
+    Logger.log(folder.getName());
+    console.log(folder.getName());
   }
 }
