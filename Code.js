9bc83ab95fb0f3c588740161d21a4f6edc264ccc
Victor Zhao <819413991zhao@gmail.com>
2024-07-01 15:39:42 +0800

    RetrievefoldersInRoot completed.

--- Code.js
+++ Code.js
@@ -1,8 +1,18 @@
 function retrieveFolders(){
+  // Retrieve all folders and display names
   let folders = DriveApp.getFolders();
   while (folders.hasNext()) {
     let folder = folders.next();
     Logger.log(folder.getName());
-    console.log(folder.getName());
+  }
+}
+
+function retrieveFoldersInRoot(){
+  // Retrieve folders within the root folder only, display names
+  let rootFolder = DriveApp.getRootFolder();
+  let folders = rootFolder.getFolders();
+  while (folders.hasNext()) {
+    let folder = folders.next();
+    Logger.log(folder.getName());
   }
 }
